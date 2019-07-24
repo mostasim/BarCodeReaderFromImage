cb7227caea8106096698f287117d238c5c458bf8
Mostasim Billah <mostasim.billah@mobioapp.com>
2019-07-24 15:01:23 +0600

    optimized and refactor

--- app/src/main/java/tech/mbsoft/barcodereader/MainActivity.java
+++ app/src/main/java/tech/mbsoft/barcodereader/MainActivity.java
@@ -1,13 +1,11 @@
 package tech.mbsoft.barcodereader;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +14,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.BinaryBitmap;
-import com.google.zxing.ChecksumException;
-import com.google.zxing.FormatException;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
-import com.google.zxing.NotFoundException;
 import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Reader;
 import com.google.zxing.Result;
--- app/src/main/java/tech/mbsoft/barcodereader/adapter/BarcodeRecyclerViewAdapter.java
+++ app/src/main/java/tech/mbsoft/barcodereader/adapter/BarcodeRecyclerViewAdapter.java
@@ -4,7 +4,6 @@ import android.arch.lifecycle.Observer;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
